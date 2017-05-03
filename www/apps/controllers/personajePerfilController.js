54ecc4a1421edd90c262b698c1b3b75f617a488f
Jose M Monsalve Diaz <josem@udel.edu>
2017-05-03 11:04:25 -0400

    Adding the option to add and remove tabs

--- www/apps/controllers/personajePerfilController.js
+++ www/apps/controllers/personajePerfilController.js
@@ -5,31 +5,25 @@
 				$scope.realizarBusqueda = function() {
 						alert("Estamos trabajando en esto! No joda!");
 				};
-                                $scope.listadoHojaServicios = `
-                                    <div id="HojaServiciosDiv"> 
-                                        <table id="hojaServiciosTable">
-                                            <tr ng-click="abrirElemento(0)"><th>Carrera</th><th>Clase</th><th>Instituci&oacute;n</th><th>Al mando de</th><th>Lugar</th><th>Fecha</th></tr>
-                                            <tr ng-click="abrirElemento(0)"><td>Carrera de armas</td><td>Una clase</td><td>Comando General</td><td>alguien</td><td>Puerto B</td><td>Enero 2016</td></tr>
-                                            <tr ng-click="abrirElemento(0)"><td>Carrera de armas</td><td>Una clase</td><td>Comando General</td><td>alguien</td><td>Puerto B</td><td>Enero 2016</td></tr>
-                                            <tr ng-click="abrirElemento(0)"><td>Carrera de armas</td><td>Una clase</td><td>Comando General</td><td>alguien</td><td>Puerto B</td><td>Enero 2016</td></tr>
-                                            <tr ng-click="abrirElemento(0)"><td>Carrera de armas</td><td>Una clase</td><td>Comando General</td><td>alguien</td><td>Puerto B</td><td>Enero 2016</td></tr>
-                                            
-                                        </table>
-                                    </div>
-                                `;
-                                $scope.tabs = [{
-                                    title: 'Listado Comisiones y trabajos',
-                                    content: $scope.listadoHojaServicios,
-                                    active: true
-                                }];
-                                
+								$scope.tabs = new Map();
+								$scope.tabsArray = [];
                                 $scope.abrirElemento = function(ElementoId) {
-                                    $scope.tabs.push( {
-                                        title: 'ComisionSeleccionada' + ($scope.tabs.length + 1),
-                                        content: 'Esta es la comision' + ElementoId,
-                                        active: true
-                                    });
+									if ( ! $scope.tabs.has(ElementoId) ) {
+                                   		 $scope.tabs.set( ElementoId, {
+                                   		     title: 'ComisionSeleccionada' + ($scope.tabs.size + 1),
+											 tabIndex: ($scope.tabs.size+1),
+                                   		     content: 'Esta es la comision' + ElementoId,
+                                   		     active: true
+                                   		 });
+									}
+									$scope.tabsArray = Array.from($scope.tabs);
                                 };
+								$scope.cerrarElemento = function(ElementoId) {
+									if ( $scope.tabs.has(ElementoId) ) {
+										$scope.tabs.delete(ElementoId);
+									}
+									$scope.tabsArray = Array.from($scope.tabs);
+								}
                                 
                 }]);
 })(window.angular);
