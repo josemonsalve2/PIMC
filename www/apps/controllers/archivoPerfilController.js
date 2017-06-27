3d8dd8c431a758b14afc46873e3961e6fa14bf98
Jose Manuel Monsalve Diaz <josem@Joses-MacBook-Pro.local>
2017-06-26 21:34:25 -0400

    agregando notas

--- www/apps/controllers/archivoPerfilController.js
+++ www/apps/controllers/archivoPerfilController.js
@@ -67,7 +67,7 @@
         $scope.notas = {};
         $scope.cargarNotas = function () {
             $http.get('http://monsalvediaz.com:5000/PIMC0.1/Consulta/ArchivosNotas?archivoID=' + $scope.archivoID).then(function (data) {
-                if (data[0:8] != "[WARNING]") {
+                if (str(data).substr(0, 8) != "[WARNING]") {
                     $scope.notas = data.data;
                     // LOG
                     console.log($scope.notas);
