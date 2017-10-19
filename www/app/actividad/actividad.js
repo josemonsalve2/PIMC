833690a9446d026b8a48f2b1dde48b48c5456134
Jose Manuel Monsalve Diaz <josem@udel.edu>
2017-10-19 07:51:41 -0400

    moviendo actividad.js y actividad.html para su propia carpeta

---
+++ www/app/actividad/actividad.js
@@ -0,0 +1,60 @@
+////////////////////////////////////////////////////////////////////////////////////
+// ACTIVIDAD PERFIL MODULE
+////////////////////////////////////////////////////////////////////////////////////
+
+(function (angular) {
+
+  'use strict';
+
+  var actividadPerfil = angular.module('actividadPerfil', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui.grid', 'ngTouch', 'ui.grid.edit', 'ui.grid.autoResize', 'ui.grid.selection', 'ui.grid.cellNav', 'xeditable']);
+  actividadPerfil.controller('actividadPerfilController', ['$scope', '$sce','$q', '$window', '$location', '$filter', '$timeout', 'i18nService', 'pimcActividadService', 'pimcBarraEstadoService', 'pimcComentarios',  function($scope, $sce, $q, $window, $location, $filter, $timeout, i18nService, pimcActividadService, pimcBarraEstadoService, pimcComentarios) {
+    $scope.actividadID = -1;
+    
+    // Funcion de inicializacion
+    var init = function() {
+      $scope.actividadID = $window.localStorage.getItem("actividadID");
+      // If not set, redirect.
+      if (!$scope.actividadID) {
+        console.log("No hay actividadID");
+        //TODO Enviar varios seleccionados
+        $window.location.href = "#!/busqueda";
+      } else {
+        if (!$scope.datosGuardados) {
+          pimcBarraEstadoService.registrarAccion("Actividad <strong>" + $scope.embarcacionID + "</strong> ha sido cargado");
+        } else {
+          pimcBarraEstadoService.registrarAccion("Actividad <strong>" + $scope.embarcacionID  + "</strong> ha sido guardado en la base de datos");
+          $scope.datosGuardados = false;
+        }
+      }
+      
+      $scope.cargarDatosPrincipales();
+      $scope.cargarNotas();
+    };
+    
+    // Datos principales
+    $scope.datosPrincipales = {};
+    $scope.cargarDatosPrincipales = function(){
+      $scope.datosPrincipales = {};
+      pimcActividadService.cargarDatosPrincipales($scope.actividadID).then( function(data) {
+        $scope.datosPrincipales = data;
+      });
+    }
+    
+    // Notas
+    $scope.notas = [];
+    $scope.notasEditado = false;
+    $scope.cargarNotas = function() {
+      pimcComentarios.cargarNotas('Actividades',$scope.actividadID).then( function(data) {
+        $scope.notas = data;
+      });
+    }
+    $scope.notificarNotasCambios = function () {
+      $scope.notasEditado = true;
+    };
+    
+    init();
+
+  }]);
+
+})(window.angular);
+
---
+++ www/app/actividad/actividadService.js
@@ -0,0 +1,99 @@
+(function (angular) {
+
+  'use strict';    
+
+  var actividadPerfil = angular.module('actividadPerfil');
+
+
+  // Service para comentarios. Cargar y guardar datos principales de la embarcacion
+  actividadPerfil.service('pimcActividadService', ['$http', '$q', 'pimcService', 'pimcBarraEstadoService', function($http, $q, pimcService, pimcBarraEstadoService, ) {
+    var actividadServiceCtrl = this;
+
+    // Funcion para cargar datos principales de la embarcacion
+    actividadServiceCtrl.cargarDatosPrincipales = function(actividadID) {
+      var consultaActividadDatosPrincipales = pimcService.crearURLOperacion('Consulta', 'Actividades');
+      var config = {
+        params: {
+          actividadID: actividadID
+        }
+      } 
+      // Cargamos los datos principales
+      return $http.get(consultaActividadDatosPrincipales, config).then( function(data) {
+        //Obtener los datos JSON
+        var actividadDatosPrincipales = data.data;
+        var datosPrincipales = {
+          contenido: {},
+          estado: pimcService.datosEstados.LIMPIO
+        };
+        // Revisamos si se recibio algo 
+        if (Object.keys(actividadDatosPrincipales).length != 0) {
+          try {
+            // Contenido, datos de embarcacion en la base de datos
+            datosPrincipales.contenido = actividadDatosPrincipales[0];
+          }
+          catch(err) {
+            console.log("Problema cargando los valores de datos principales de la actividad " + err.message);
+          }
+        }
+
+      });
+    }; //Fin de cargar datos principales
+
+    actividadServiceCtrl.guardarDatosPrincipales = function(datosPrincipales) {
+
+      // Si los datos fueron cambiados, entonces actualizamos todas las bases de datos
+      if (datosPrincipales.estado != pimcService.datosEstados.LIMPIO) {
+        pimcBarraEstadoService.registrarAccion("Actualizando BD Actividad");
+        var modificarActividadesURL = pimcService.crearURLOperacion('Modificar', 'Actividades');
+        var parametros = datosPrincipales.contenido;
+        parametros.idUnico = 'actividadID';
+        var config = {
+          params: parametros
+        }
+        return $http.get(modificarActividadesURL, config).then( 
+          // funcion conexion exitosa
+          function (data) {
+            if (data.data[0] != 0) {
+              return true;
+
+            } else {
+              pimcService.debug("ERROR no se modificó la base de datos guardando datos principales de actividades", data);
+              return false;
+            }
+          }, function (dataError) {
+            // funcion error de conexion
+            pimcService.debug("ERROR guardando actividades", dataError);
+            return $q.resolve(false);
+          }
+        );
+      }
+    }; // Fin de guardar datos principales
+    
+    actividadServiceCtrl.crearActividad = function(actividadNueva) {
+      pimcBarraEstadoService.registrarAccion("Actualizando BD Actividad");
+        var crearActividadesURL = pimcService.crearURLOperacion('Insertar', 'Actividades');
+        var parametros = actividadNueva;
+        var config = {
+          params: parametros
+        }
+        return $http.get(crearActividadesURL, config).then( 
+          // funcion conexion exitosa
+          function (data) {
+            if (data.data[0] != 0) {
+              return true;
+            } else {
+              pimcService.debug("ERROR no se modificó la base de datos guardando datos principales de actividades", data);
+              return false;
+            }
+          }, function (dataError) {
+            // funcion error de conexion
+            pimcService.debug("ERROR guardando actividades", dataError);
+            return $q.resolve(false);
+          }
+        );
+      }
+    }
+  }]);
+
+
+})(window.angular);
