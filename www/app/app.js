d0fd6584ece33d3b5d4acf2e9f27ae4b8dfd2773
Jose Manuel Monsalve Diaz <josem@Joses-MacBook-Pro.local>
2017-11-20 01:24:56 -0500

    Parte del servicio menu. correccion

--- www/app/app.js
+++ www/app/app.js
@@ -5,6 +5,7 @@
   var pimc = angular.module('pimc', [
     'ngRoute',
     'indexModule',
+    'pimcMenuModule',
     'archivoPerfil',
     'personajePerfil',
     'embarcacionPerfil',
@@ -80,23 +81,5 @@
         return gridOptions;
     }]);
   });
-
-  pimc.directive('setParentActive', ['$location', function($location) {
-    return {
-      restrict: 'A',
-      link: function(scope, element, attrs, controller) {
-        var classActive = attrs.setParentActive || 'active',
-            path = attrs.ngHref.replace('#!', '');
-        scope.location = $location;
-        scope.$watch('location.path()', function(newPath) {
-          if (path == newPath) {
-            element.parent().addClass(classActive);
-          } else {
-            element.parent().removeClass(classActive);
-          }
-        })
-      }
-    }
-  }]);
 
 })(window.angular);
