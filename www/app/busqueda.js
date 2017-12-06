b696ee83e1cd040723b4db662e9fe863d3b4bc2b
Jose M Monsalve Diaz <josem@udel.edu>
2017-12-06 02:19:43 -0500

    Mostrar contador de documentos en la aplicacion

--- www/app/busqueda.js
+++ www/app/busqueda.js
@@ -38,6 +38,10 @@
         // Cargando los datos
         $scope.cargandoDatos = true;
 
+        // Numero de filas 
+        $scope.numeroFilasVisibles = 0;
+        $scope.numeroFilasTotales = 0;
+
         //Tabla de resultados
         $scope.tablaResultados = {
             enableRowSelection: true,
@@ -61,12 +65,16 @@
         ];
         $scope.tablaResultados.onRegisterApi = function (gridApi) {
             $scope.tablaResultadosGridApi = gridApi;
+            $scope.tablaResultadosGridApi.core.on.rowsRendered( $scope, function (){
+                $scope.numeroFilasVisibles = $scope.tablaResultadosGridApi.core.getVisibleRows().length;
+            });
         };
         
         $scope.init = function() {
             var consultaTodosDocumentos = pimcService.crearURLOperacion('ConsultarTodos', 'Documentos');
             // Cargamos los Documentos
             return $http.get(consultaTodosDocumentos).then(function (data) {
+                $scope.numeroFilasTotales = data.data.length;
                 data.data.forEach(function changeDates(row, index) {
                     if (row.fechaInicial !== null) {
                         row.fechaInicial = (row.fechaInicial && row.fechaInicial.length != 0) ? $filter('date')(new Date(row.fechaInicial), String(row.fechaInicialFormato).toLowerCase(), 'UTC') : null;
