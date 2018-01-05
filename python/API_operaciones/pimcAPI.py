c243e716e6a212fba79cb455444bb5f6390886e9
Jose Manuel Monsalve Diaz <josem@Joses-MacBook-Pro.local>
2018-01-05 10:44:42 -0500

    Arreglamos indent para 4 espacios por estandard python

--- python/API_operaciones/pimcAPI.py
+++ python/API_operaciones/pimcAPI.py
@@ -6,23 +6,22 @@ from API_operaciones.modificar import modificarElemento
 #from API_operaciones.autocompletar import autocompletar
 
 class pimc:
-  
-  def insertarNuevoElemento(self, elementoRelacional, parametrosJSON):
-    insertarNuevoElemento(elementoRelacional, parametrosJSON)
-    
-  def consultarElemento(self, elementoRelacional, parametrosJSON):
-    consultarElemento(elementoRelacional, parametrosJSON)
-  
-  def consultarTodosFiltro(self, elementoRelacional, parametrosJSON):
-    consultarTodosFiltro(elementoRelacional, parametrosJSON)
-  
-  def eliminarElemento(self, elementoRelacional, parametrosJSON):
-    eliminarElemento(elementoRelacional, parametrosJSON)
-  
-  def modificarElemento(self, elementoRelacional, parametrosJSON):
-    modificarElemento(elementoRelacional, parametrosJSON)
-  
-  def autocompletarConsulta(self):
-    return
-  
+    def insertarNuevoElemento(self, elementoRelacional, parametrosJSON):
+        insertarNuevoElemento(elementoRelacional, parametrosJSON)
+
+    def consultarElemento(self, elementoRelacional, parametrosJSON):
+        consultarElemento(elementoRelacional, parametrosJSON)
+
+    def consultarTodosFiltro(self, elementoRelacional, parametrosJSON):
+        consultarTodosFiltro(elementoRelacional, parametrosJSON)
+
+    def eliminarElemento(self, elementoRelacional, parametrosJSON):
+        eliminarElemento(elementoRelacional, parametrosJSON)
+
+    def modificarElemento(self, elementoRelacional, parametrosJSON):
+        modificarElemento(elementoRelacional, parametrosJSON)
+
+    def autocompletarConsulta(self):
+        return
+
 pimcAPI = pimc()
