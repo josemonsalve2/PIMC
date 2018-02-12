9a60395c59f94ce48f343bfcae9447d63dbb0371
Jose Manuel Monsalve Diaz <josem@Joses-MacBook-Pro.local>
2018-02-11 19:38:28 -0500

    Creamos tres nuevas operaciones para pimcBaseDatos. Crear, insertar y eliminar elemento. Mucho mas facil y modular que usar http directamente

--- www/app/global/baseDatos/pimcBaseDatos.js
+++ www/app/global/baseDatos/pimcBaseDatos.js
@@ -45,6 +45,67 @@
                 return valoresElementoRelacional;
             });
         }
+        pimcBaseDatosServiceCtrl.insertarElemento = function(baseDatos, contenido) {
+            if (! (contenido instanceof Object)) {
+                return $q.reject("contenido debe ser un diccionario");
+            }
+            var insertarElementoURL = pimcService.crearURLOperacion('Insertar', baseDatos);
+            return $http.post(insertarElementoURL, contenido).then(
+                function (data) {
+                    // Revisamos si la respuesta tiene elementos 
+                    if (Object.keys(data.data[0]).length != 0){
+                        return data.data[0];
+                    }
+                    // No deberia llegar aca
+                    $q.reject("Error al insertar nuevo elemento relacional");
+                }, 
+                function (error) {
+                    $q.reject("Error al insertar elemento en la base de datos " + baseDatos);
+                }
+            );
+        }
+        pimcBaseDatosServiceCtrl.modificarElemento = function (baseDatos, contenido) {
+            if (! (contenido instanceof Object)) {
+                return $q.reject("contenido debe ser un diccionario");
+            }
+            var modificarElementoURL = pimcService.crearURLOperacion('Modificar', baseDatos);
+            return $http.post(modificarElementoURL, contenido).then(
+                function (data) {
+                    // Revisamos si la respuesta tiene elementos 
+                    if (data.data[0] != 0){
+                        return $q.reject("No se modifico nada en la base de datos");
+                    } else {
+                        return $q.resolve(data.data[0]);
+                    }
+                    // No deberia llegar aca
+                    $q.reject("Error al modificar elemento relacional");
+                }, 
+                function (error) {
+                    $q.reject("Error al modificar elemento en la base de datos " + baseDatos);
+                }
+            );
+        }
+        pimcBaseDatosServiceCtrl.eliminarElemento = function (baseDatos, contenido) {
+            if (! (contenido instanceof Object)) {
+                return $q.reject("contenido debe ser un diccionario");
+            }
+            var modificarElementoURL = pimcService.crearURLOperacion('Eliminar', baseDatos);
+            return $http.post(modificarElementoURL, contenido).then(
+                function (data) {
+                    // Revisamos si la respuesta tiene elementos 
+                    if (data.data[0] != 0){
+                        return $q.reject("No se elimino nada en la base de datos");
+                    } else {
+                        return $q.resolve(data.data[0]);
+                    }
+                    // No deberia llegar aca
+                    $q.reject("Error al eliminar elemento relacional");
+                }, 
+                function (error) {
+                    $q.reject("Error al eliminar elemento en la base de datos " + baseDatos);
+                }
+            );
+        }
 
     }]);
 })(window.angular);
